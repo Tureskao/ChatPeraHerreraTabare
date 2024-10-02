3404d027093afe7a1847f0f547ee402f0a6f8716
Genaro Tomas Tabare <gtureskao@hotmail.com>
2024-10-02 18:06:57 -0300

    TSDS-Chat-02 Primera implementacion de TCP-ServidorChatForm

---
+++ ChatServidor-PeraHerreraTabare/TCP-ServidorChatForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace ChatServidor_PeraHerreraTabare
+{
+    partial class TCP_ServidorChatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mostrarTextBox = new System.Windows.Forms.TextBox();
+            this.entradaTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // mostrarTextBox
+            // 
+            this.mostrarTextBox.Enabled = false;
+            this.mostrarTextBox.Location = new System.Drawing.Point(13, 13);
+            this.mostrarTextBox.Multiline = true;
+            this.mostrarTextBox.Name = "mostrarTextBox";
+            this.mostrarTextBox.Size = new System.Drawing.Size(750, 450);
+            this.mostrarTextBox.TabIndex = 0;
+            // 
+            // entradaTextBox
+            // 
+            this.entradaTextBox.Location = new System.Drawing.Point(13, 480);
+            this.entradaTextBox.Multiline = true;
+            this.entradaTextBox.Name = "entradaTextBox";
+            this.entradaTextBox.Size = new System.Drawing.Size(647, 46);
+            this.entradaTextBox.TabIndex = 1;
+            // 
+            // TCP_ServidorChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.entradaTextBox);
+            this.Controls.Add(this.mostrarTextBox);
+            this.Name = "TCP_ServidorChatForm";
+            this.Text = "TCP_ServidorChatForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mostrarTextBox;
+        private System.Windows.Forms.TextBox entradaTextBox;
+    }
+}
---
+++ ChatServidor-PeraHerreraTabare/TCP-ServidorChatForm.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ChatServidor_PeraHerreraTabare
+{
+    public partial class TCP_ServidorChatForm : Form
+    {
+        //Constructor vacío
+        public TCP_ServidorChatForm()
+        {
+            InitializeComponent();
+        }
+
+        private Socket conexion; //Socket para aceptar una conexión
+        private Thread lecturaThread; // Thread para procesar los mensajes entrantes
+        private NetworkStream socketStream; //Flujo para datos de la red
+        private BinaryWriter escritor; //Facilita la escritura en el flujo
+        private BinaryReader lector; //Facilita la lectura del flujo
+
+        // Inicializa el hilo para la lectura
+        private void TCP_ServidorChatForm_Load(object sender, EventArgs e)
+        {
+            lecturaThread = new Thread(new ThreadStart(EjecutarServidor));
+            lecturaThread.Start();
+        }
+
+        // Cierra todos los subprocesos asociados con esta aplicacion
+        private void TCP_ServidorChatForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Environment.Exit(System.Environment.ExitCode);
+        }
+
+        // Delegado que permite que se haga una llamada al método MostrarMensaje en el hilo
+        // que crea y mantiene la GUI
+        private delegate void displayDelegate(string mensaje);
+
+        // El método DisplayDelegate establece la propiedad Text de mostrarTextBox
+        // en forma segura para los subprocesos
+        private void MostrarMensaje(string mensaje)
+        {
+            if (mostrarTextBox.InvokeRequired)
+            {
+                Invoke(new displayDelegate(MostrarMensaje),
+                    new object[] { mensaje });
+            }
+            else
+            {
+                mostrarTextBox.Text += mensaje;
+            }
+        }
+
+        
+        // Delegado que permite llamar al método DeshabilitarSalida
+        // en el subproceso que crea y mantiene la GUI
+        private delegate void DisableInputDelegate(bool value);
+
+        // El método DeshabilitarEntrada establece la propiedad ReadOnly de entradaTextBox
+        // de una manera segura para los subprocesos
+        private void DeshabilitarEntrada(bool valor)
+        {
+            if (entradaTextBox.InvokeRequired)
+            {
+                Invoke(new DisableInputDelegate(DeshabilitarEntrada),
+                    new object[] { valor });
+            }
+
+            else
+            {
+                entradaTextBox.ReadOnly = valor;
+            }
+        }
+
+    // Envía al cliente el texto escrito en el Servidor
+    private void entradaTextBox_KeyDown(object sender, KeyEventArgs e)
+    {
+        // Envía el texto al Cliente
+        try
+        {
+            if (e.KeyCode == Keys.Enter && entradaTextBox.ReadOnly == false)
+            {
+                escritor.Write("SERVIDOR>>> " + entradaTextBox.Text);
+                mostrarTextBox.Text += "\r\nSERVIDOR>>> " + entradaTextBox.Text;
+                if (entradaTextBox.Text == "TERMINAR")
+                {
+                    conexion.Close();
+                }
+                entradaTextBox.Clear();
+            }
+        }
+        catch (SocketException)
+        {
+            mostrarTextBox.Text += "\nError al escribir objeto";
+        }
+    }
+
+    public void EjecutarServidor()
+    {
+        TcpListener oyente;
+        int contador = 1;
+        try
+        {
+            // Paso 1
+            IPAddress local = IPAddress.Parse("127.0.0.1");
+            oyente = new TcpListener(local, 50000);
+
+            // Paso 2
+            oyente.Start();
+
+            // Paso 3
+            while (true)
+            {
+                MostrarMensaje("Esperando una conexión \r\n");
+
+                conexion = oyente.AcceptSocket();
+                socketStream = new NetworkStream(conexion);
+                escritor = new BinaryWriter(socketStream);
+                lector = new BinaryReader(socketStream);
+
+                MostrarMensaje("Conexion " + contador + " recibida.\r\n");
+
+                escritor.Write("SERVIDOR>>> Conexión exitosa");
+
+                DeshabilitarEntrada(false); //habilita entradaTextBox
+
+                string laRespuesta = "";
+
+                // Paso 4
+                do
+                {
+                    try
+                    {
+                        laRespuesta = lector.ReadString();
+                        MostrarMensaje("\r\n" + laRespuesta);
+                    }
+                    catch (Exception)
+                    {
+                            break;
+                    }
+                } while (laRespuesta != "CLIENTE>>> TERMINAR" && conexion.Connected);
+
+                MostrarMensaje("\r\nEl usuario terminó la conexión\r\n");
+
+                //Paso 5
+                escritor.Close();
+                lector.Close();
+                socketStream.Close();
+                conexion.Close();
+
+                DeshabilitarEntrada(true);
+                contador++;
+            }
+        }
+        catch (Exception error)
+        {
+            MessageBox.Show(error.ToString());
+        }
+    }
+}
+
+}
